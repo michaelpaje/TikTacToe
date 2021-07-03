cd6743314de4d1fc5ccca4645f1160d8f2e414e0
cams <camille.monis@gmail.com>
2021-07-03 16:33:48 +0800

    about page base grgrgr

---
+++ TikTacToe/AboutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutViewController.swift
+//  TikTacToe
+//
+//  Created by macstudent on 7/3/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello")
+    }
+    
+
+    
+
+}
