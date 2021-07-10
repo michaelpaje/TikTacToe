797c158a502128310770d96e686237adb1e9533b
rinzuno <michaelpaje99@gmail.com>
2021-07-10 19:47:30 +0800

    fixed all bugs on login & register

--- TikTacToe/Login.swift
+++ TikTacToe/Login.swift
@@ -10,24 +10,29 @@ import UIKit
 class Login: UIViewController {
 
     let defaults = UserDefaults.standard
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-        }
-        
-        @IBOutlet weak var UnameField: UITextField!
-        @IBOutlet weak var PwordField: UITextField!
     var success = false
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var UnameField: UITextField!
+    @IBOutlet weak var PwordField: UITextField!
     
-    @IBOutlet weak var result: UILabel!
     
     @IBAction func loginbtn(_ sender: UIButton) {
         let size = defaults.integer(forKey: "size")
         for i in 0...size {
             let tempUser = defaults.string(forKey: "Username" + String(i))
             let tempPass = defaults.string(forKey: "Password" + String(i))
-            if(tempUser == UnameField.text! && tempPass == PwordField.text!) {
+            if(UnameField.text == "" && PwordField.text == "") {
+                let tfMessage = UIAlertController(title: "Login Failed", message: "Please fill up the text fields", preferredStyle: .alert)
+                let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+                })
+                tfMessage.addAction(ok)
+                self.present(tfMessage, animated: true, completion: nil)
+            }
+            else if(tempUser == UnameField.text! && tempPass == PwordField.text!) {
                 defaults.set(tempUser,forKey: "usernamelogin")
                 defaults.set(tempPass,forKey: "passwordlogin")
                 defaults.set(String(i),forKey: "num")
@@ -36,6 +41,9 @@ class Login: UIViewController {
             }
         }
         if success {
+            success = false
+            UnameField.text = ""
+            PwordField.text = ""
             let vc = storyboard?.instantiateViewController(identifier: "tabBar") as! UITabBarController
             present(vc, animated: true)
         }
@@ -47,7 +55,7 @@ class Login: UIViewController {
             dialogMessage.addAction(ok)
             self.present(dialogMessage, animated: true, completion: nil)
             }
-            
+            PwordField.text = ""
         }
         
         @IBAction func registerbtn(_ sender: UIButton) {
--- TikTacToe/Register.swift
+++ TikTacToe/Register.swift
@@ -21,6 +21,7 @@ class Register: UIViewController {
     @IBOutlet weak var PwordReg: UITextField!
     @IBOutlet weak var ConfirmPass: UITextField!
     var size = 0
+    // check if existing
     func check(_ UnameReg: String, _ sz: Int) -> Bool {
         for i in 0...sz {
             let tempStr = defaults.string(forKey: "Username" + String(i))
@@ -32,70 +33,82 @@ class Register: UIViewController {
     }
     
     @IBAction func ConfirmReg(_ sender: Any) {
+        let successMessage = UIAlertController(title: "Registered Successfully!", message: "Welcome!", preferredStyle: .alert)
         // store if existing
         if defaults.object(forKey: "size") != nil {
             size = defaults.integer(forKey: "size")
         }
-        if UnameReg.text?.isEmpty == true || PwordReg.text?.isEmpty == true {
+        if UnameReg.text?.isEmpty == true || PwordReg.text?.isEmpty == true || ConfirmPass.text?.isEmpty == true {
             let errorMessage = UIAlertController(title: "Register Failed", message: "Make sure all information is filled", preferredStyle: .alert)
+                    
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+                })
+                    
+            //Add OK button to a dialog message
+            errorMessage.addAction(ok)
+            // Present Alert to
+            self.present(errorMessage, animated: true, completion: nil)
+        }
+        else if (PwordReg.text != ConfirmPass.text) {
+            let errorMessage = UIAlertController(title: "Register Failed", message: "Passwords should be the same", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+                })
+            errorMessage.addAction(ok)
+            self.present(errorMessage, animated: true, completion: nil)
+        }
+        else {
+            if size == 0 {
+                defaults.set(UnameReg.text, forKey: "Username" + String(size))
+                    
+                defaults.set(PwordReg.text, forKey: "Password" + String(size))
+                defaults.set(size+1, forKey: "size")
+                defaults.set(0,forKey: "Username" + String(size) + "winr")
+                defaults.set(0,forKey: "Username" + String(size) + "drawr")
+                defaults.set(0,forKey: "Username" + String(size) + "loser")
+                let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                    print("Ok button tapped")
+                    self.dismiss (animated: true, completion: nil)
+                    })
+                successMessage.addAction(ok)
+                self.present(successMessage, animated: true, completion: nil)
+            } else {
+                if check(UnameReg.text!, size) {
+                    defaults.set(UnameReg.text, forKey: "Username" + String(size))
+                    defaults.set(PwordReg.text, forKey: "Password" + String(size))
+                    defaults.set(0,forKey: "Username" + String(size) + "winr")
+                    defaults.set(0,forKey: "Username" + String(size) + "drawr")
+                    defaults.set(0,forKey: "Username" + String(size) + "loser")
+                    defaults.set(size+1, forKey: "size")
+                    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                        print("Ok button tapped")
+                        self.dismiss (animated: true, completion: nil)
+                        })
+                    successMessage.addAction(ok)
+                    self.present(successMessage, animated: true, completion: nil)
+                    
+                    
+                } else {
+                    let userEx = UIAlertController(title: "Register Failed", message: "Username already exist!", preferredStyle: .alert)
                     
                     // Create OK button with action handler
                     let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
                         print("Ok button tapped")
-                     })
-                    
+                        })
+                                    
                     //Add OK button to a dialog message
-                    errorMessage.addAction(ok)
+                    userEx.addAction(ok)
                     // Present Alert to
-                    self.present(errorMessage, animated: true, completion: nil)
+                    self.present(userEx, animated: true, completion: nil)
                 }
-               else if (PwordReg.text != ConfirmPass.text) {
-                    let errorMessage = UIAlertController(title: "Register Failed", message: "Passwords should be the same", preferredStyle: .alert)
-                    
-                    let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
-                        print("Ok button tapped")
-                     })
-                    
-                    errorMessage.addAction(ok)
-                    self.present(errorMessage, animated: true, completion: nil)
-                }
-               else {
-                    if size == 0 {
-                        defaults.set(UnameReg.text, forKey: "Username" + String(size))
-                        defaults.set(PwordReg.text, forKey: "Password" + String(size))
-                        defaults.set(size+1, forKey: "size")
-                        defaults.set(0,forKey: "Username" + String(size) + "winr")
-                        defaults.set(0,forKey: "Username" + String(size) + "drawr")
-                        defaults.set(0,forKey: "Username" + String(size) + "loser")
-                        let vc = storyboard?.instantiateViewController(identifier: "login_vc") as! Login
-                        present(vc, animated: true)
-                    } else {
-                        if check(UnameReg.text!, size) {
-                            defaults.set(UnameReg.text, forKey: "Username" + String(size))
-                            defaults.set(PwordReg.text, forKey: "Password" + String(size))
-                            defaults.set(0,forKey: "Username" + String(size) + "winr")
-                            defaults.set(0,forKey: "Username" + String(size) + "drawr")
-                            defaults.set(0,forKey: "Username" + String(size) + "loser")
-                            defaults.set(size+1, forKey: "size")
-                            let vc = storyboard?.instantiateViewController(identifier: "login_vc") as! Login
-                            present(vc, animated: true)
-                        }
-                    }
-                }
+            }
+        }
     }
 
-    
-    
     @IBAction func CancelReg(_ sender: Any) {
-        dismiss (animated: true, completion: nil)    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        dismiss (animated: true, completion: nil)
+        
     }
-    */
 }
